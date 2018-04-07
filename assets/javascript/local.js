3fc1691889a28c31ffbe28ff683b92fb871acfa4
Scott Peterson <scott.peterson.va@gmail.com>
2018-04-07 16:54:04 -0400

    fixed logo, added projects, working on collapsible menu

---
+++ assets/javascript/local.js
@@ -0,0 +1,9 @@
+// $( document ).ready(function)
+
+// var elem = document.querySelector('.modal');
+//   var instance = M.Modal.init(elem, options);
+
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+     
