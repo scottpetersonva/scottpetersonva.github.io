d08bbfcb33c7196ea56b31a68a6dca32fc47848d
Scott Peterson <scott.peterson.va@gmail.com>
2019-11-09 11:55:46 -0500

    added spirit mountain experience

--- assets/javascript/local.js
+++ assets/javascript/local.js
@@ -2,8 +2,14 @@ $( document ).ready(function(){
   $('.collapsible').collapsible();
   $('.slider').slider();
   $('.modal').modal();
+  $('.scrollspy').scrollSpy();
   $('.button-collapse').sideNav({
         closeOnClick: true
       }
     );
+  });
+
+  $( document ).ready(function(){
+  ScrollReveal().reveal('.reveal', { delay: 500 });
+
   });
